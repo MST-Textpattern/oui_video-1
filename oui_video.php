f2d89a7e83e108e12e8d1b7ec492096b408c7011
Nicolas Morand <contact@nicolasmorand.com>
2018-07-04 12:34:51 +0200

    Provider dedicated tags are now declared in the parent class

--- oui_video.php
+++ oui_video.php
@@ -159,13 +159,3 @@ namespace Oui {
         }
     }
 }
-
-namespace {
-    function oui_video($atts) {
-        return oui_player(array_merge(array('provider' => 'video'), $atts));
-    }
-
-    function oui_if_video($atts, $thing) {
-        return oui_if_player(array_merge(array('provider' => 'video'), $atts), $thing);
-    }
-}
